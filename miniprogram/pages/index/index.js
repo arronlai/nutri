b8fdbcad50abbb81deefd3d4ee1a5a252db65fdd
arron.laihongwei <arron.laihongwei@gmail.com>
2024-02-22 16:01:26 +0800

    feat: UI for 1 move

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,71 +1,27 @@
 // index.js
 // const app = getApp()
-const { envList } = require('../../envList.js');
 
 Page({
   data: {
     showUploadTip: false,
-    powerList: [{
-      title: '云函数',
-      tip: '安全、免鉴权运行业务代码',
-      showItem: false,
-      item: [{
-        title: '获取OpenId',
-        page: 'getOpenId'
+    powerList: [
+      {
+        id: 0, // 动作id
+        title: '杠铃卧推', // 动作名称
+        pic: 'https://img.net/1.gif', // 动作图
+        showItem: false,
+        item: [
+          {
+            weight: 10,
+            unit: 'kg',
+            times: 10,
+            rest: 60,
+            completed: false,
+          },
+        ],
       },
-      //  {
-      //   title: '微信支付'
-      // },
-       {
-        title: '生成小程序码',
-        page: 'getMiniProgramCode'
-      },
-      // {
-      //   title: '发送订阅消息',
-      // }
-    ]
-    }, {
-      title: '数据库',
-      tip: '安全稳定的文档型数据库',
-      showItem: false,
-      item: [{
-        title: '创建集合',
-        page: 'createCollection'
-      }, {
-        title: '更新记录',
-        page: 'updateRecord'
-      }, {
-        title: '查询记录',
-        page: 'selectRecord'
-      }, {
-        title: '聚合操作',
-        page: 'sumRecord'
-      }]
-    }, {
-      title: '云存储',
-      tip: '自带CDN加速文件存储',
-      showItem: false,
-      item: [{
-        title: '上传文件',
-        page: 'uploadFile'
-      }]
-    },{
-      title: '云模板',
-      tip: '基于页面模板，快速配置、搭建小程序页面',
-      tag: 'new',
-      page: 'singleTemplate',
-    }, {
-      title: '云后台',
-      tip: '开箱即用的小程序后台管理系统',
-      page: 'cloudBackend',
-    }, {
-      title: '云托管',
-      tip: '不限语言的全托管容器服务',
-      link: 'https://cloud.weixin.qq.com/cloudrun',
-    }],
-    envList,
-    selectedEnv: envList[0],
-    haveCreateCollection: false
+    ],
+    haveCreateCollection: false,
   },
 
   onClickPowerInfo(e) {
@@ -81,76 +37,15 @@ Page({
       wx.navigateTo({
         url: `/pages/${selectedItem.page}/index`,
       });
-    } else if (selectedItem.title === '数据库' && !this.data.haveCreateCollection) {
+    } else if (
+      selectedItem.title === '数据库' &&
+      !this.data.haveCreateCollection
+    ) {
       this.onClickDatabase(powerList);
     } else {
       this.setData({
-        powerList
+        powerList,
       });
     }
   },
-
-  onChangeShowEnvChoose() {
-    wx.showActionSheet({
-      itemList: this.data.envList.map(i => i.alias),
-      success: (res) => {
-        this.onChangeSelectedEnv(res.tapIndex);
-      },
-      fail (res) {
-        console.log(res.errMsg);
-      }
-    });
-  },
-
-  onChangeSelectedEnv(index) {
-    if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
-      return;
-    }
-    const powerList = this.data.powerList;
-    powerList.forEach(i => {
-      i.showItem = false;
-    });
-    this.setData({
-      selectedEnv: this.data.envList[index],
-      powerList,
-      haveCreateCollection: false
-    });
-  },
-
-  jumpPage(e) {
-    wx.navigateTo({
-      url: `/pages/${e.currentTarget.dataset.page}/index?envId=${this.data.selectedEnv.envId}`,
-    });
-  },
-
-  onClickDatabase(powerList) {
-    wx.showLoading({
-      title: '',
-    });
-    wx.cloud.callFunction({
-      name: 'quickstartFunctions',
-      config: {
-        env: this.data.selectedEnv.envId
-      },
-      data: {
-        type: 'createCollection'
-      }
-    }).then((resp) => {
-      if (resp.result.success) {
-        this.setData({
-          haveCreateCollection: true
-        });
-      }
-      this.setData({
-        powerList
-      });
-      wx.hideLoading();
-    }).catch((e) => {
-      console.log(e);
-      this.setData({
-        showUploadTip: true
-      });
-      wx.hideLoading();
-    });
-  }
 });
